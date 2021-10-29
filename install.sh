2d204cb0151483afda23c73faf86d0533d2a455d
pvdinh089 <pvdinh089@gmail.com>
2021-10-29 16:19:58 +0700

    Update

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ mkswap /swapfile
 swapon /swapfile
 echo "/swapfile none swap default 0 0" >> /etc/fstab
 
+printf "\e[1;32m ĐÃ TẠO XONG SWAP \e[0m"
+printf "\e[1;32m =================================== \e[0m"
+sleep 5
+
 # Đặt múi giờ và mã unicode
 ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 hwclock --systohc
@@ -15,12 +19,20 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 sed -i '177s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+printf "\e[1;32m ĐÃ ĐẶT XONG GIỜ VÀ UTF-8 \e[0m"
+printf "\e[1;32m =================================== \e[0m"
+sleep 5
 
 # Cấu hình mạng
 echo "ArchLinux" >> /etc/hostname   #ArchLinux là tên PC
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 ArchLinux.localdomain ArchLinux" >> /etc/hosts
+cat /etc/hosts
+
+printf "\e[1;32m CẤU HÌNH MẠNG THÀNH CÔNG \e[0m"
+printf "\e[1;32m =================================== \e[0m"
+sleep 5
 
 # Đặt mật khẩu cho user root
 echo root:260691 | chpasswd
@@ -30,15 +42,24 @@ useradd -m -g wheel dinhpv          #dinhpv là tên người dùng
 echo dinhpv:260691 | chpasswd
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers.d/dinhpv
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/dinhpv
+printf "\e[1;32m ĐÃ TẠO XONG NGƯỜI DÙNG \e[0m"
+printf "\e[1;32m =================================== \e[0m"
+sleep 5
 
 # Cài đặt các gói cơ bản
 pacman -S --noconfirm grub efibootmgr os-prober ntfs-3g mtools 
 pacman -S --noconfirm networkmanager network-manager-applet dialog wpa_supplicant dosfstools texinfo inetutils netctl dhcpcd linux-headers
+printf "\e[1;32m XONG CÀI ĐẶT CÁC GÓI CƠ BẢN \e[0m"
+printf "\e[1;32m =================================== \e[0m"
+sleep 5
 
 # Cài đặt GRUB
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub 
 grub-mkconfig -o /boot/grub/grub.cfg
+printf "\e[1;32m HOÀN THÀNH CÀI ĐẶT VÀ CẤU HÌNH GRUB \e[0m"
+printf "\e[1;32m =================================== \e[0m"
+sleep 5
 
 # Cài đặt thêm các ứng dụng khác trợ khác
 sudo pacman -S --noconfirm bluez bluez-utils alsa-utils pulseaudio firefox terminator nautilus git terminus-font ttf-ubuntu-font-family ttf-dejavu
