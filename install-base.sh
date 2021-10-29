2a197e8b2308ec0c246283fde66929388516a91a
pvdinh089 <pvdinh089@gmail.com>
2021-10-29 17:01:27 +0700

    Update grub install directory

--- install-base.sh
+++ install-base.sh
@@ -1,11 +1,11 @@
 # Cài đặt các gói cơ bản
 pacstrap /mnt base base-devel linux linux-firmware vim intel-ucode
-printf "\e[1;32m CÀI XONG CÁC GÓI CƠ BẢN \e[0m"
+printf "\e[1;32m CÀI XONG CÁC GÓI CƠ BẢN \e[0m \n"
 sleep 10
 # Cấu hình hệ thống
 genfstab -U /mnt >> /mnt/etc/fstab
-printf "\e[1;32m CẤU HÌNH HỆ THỐNG THÀNH CÔNG \e[0m"
+printf "\e[1;32m CẤU HÌNH HỆ THỐNG THÀNH CÔNG \e[0m \n"
 sleep 5
 # Chuyển sang người dùng root
 arch-chroot /mnt
-printf "\e[1;32m ĐÃ CHUYỂN SANG NGƯỜI DÙNG ROOT \e[0m"
+printf "\e[1;32m ĐÃ CHUYỂN SANG NGƯỜI DÙNG ROOT \e[0m \n"
--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#@author:dinhpv
 
 # Tạo swapfile
 fallocate -l 5GB /swapfile
@@ -9,7 +8,7 @@ swapon /swapfile
 echo "/swapfile none swap default 0 0" >> /etc/fstab
 
 printf "\e[1;32m ĐÃ TẠO XONG SWAP \e[0m"
-printf "\e[1;32m =================================== \e[0m"
+printf "\e[1;32m =================================== \e[0m \n"
 sleep 5
 
 # Đặt múi giờ và mã unicode
@@ -20,7 +19,7 @@ sed -i '177s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 printf "\e[1;32m ĐÃ ĐẶT XONG GIỜ VÀ UTF-8 \e[0m"
-printf "\e[1;32m =================================== \e[0m"
+printf "\e[1;32m =================================== \e[0m \n"
 sleep 5
 
 # Cấu hình mạng
@@ -31,7 +30,7 @@ echo "127.0.1.1 ArchLinux.localdomain ArchLinux" >> /etc/hosts
 cat /etc/hosts
 
 printf "\e[1;32m CẤU HÌNH MẠNG THÀNH CÔNG \e[0m"
-printf "\e[1;32m =================================== \e[0m"
+printf "\e[1;32m =================================== \e[0m \n"
 sleep 5
 
 # Đặt mật khẩu cho user root
@@ -43,37 +42,39 @@ echo dinhpv:260691 | chpasswd
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers.d/dinhpv
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/dinhpv
 printf "\e[1;32m ĐÃ TẠO XONG NGƯỜI DÙNG \e[0m"
-printf "\e[1;32m =================================== \e[0m"
+printf "\e[1;32m =================================== \e[0m \n"
 sleep 5
 
 # Cài đặt các gói cơ bản
 pacman -S --noconfirm grub efibootmgr os-prober ntfs-3g mtools 
 pacman -S --noconfirm networkmanager network-manager-applet dialog wpa_supplicant dosfstools texinfo inetutils netctl dhcpcd linux-headers
 printf "\e[1;32m XONG CÀI ĐẶT CÁC GÓI CƠ BẢN \e[0m"
-printf "\e[1;32m =================================== \e[0m"
+printf "\e[1;32m =================================== \e[0m \n"
 sleep 5
 
 # Cài đặt GRUB
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub 
 grub-mkconfig -o /boot/grub/grub.cfg
-printf "\e[1;32m HOÀN THÀNH CÀI ĐẶT VÀ CẤU HÌNH GRUB \e[0m"
-printf "\e[1;32m =================================== \e[0m"
+printf "\e[1;32m HOÀN THÀNH CÀI ĐẶT VÀ CẤU HÌNH GRUB \e[0m \n"
+printf "\e[1;32m =================================== \e[0m \n"
 sleep 5
 
 # Cài đặt thêm các ứng dụng khác trợ khác
 sudo pacman -S --noconfirm bluez bluez-utils alsa-utils pulseaudio firefox terminator nautilus git terminus-font ttf-ubuntu-font-family ttf-dejavu
 
-# Mở các dịch vụ cơ bản internet và bluetooth
-systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable dhcpcd.service
-
 # Cài đặt Driver card màn hình NVIDIA
 pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 # pacman -S --noconfirm xf86-video-amdgpu
 
-printf "\e[1;32m Đã hoàn thành cài đặt! gõ exit, umount -a và reboot.\e[0m"
+# Mở các dịch vụ cơ bản internet và bluetooth
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable dhcpcd.service
+printf "\e[1;32m MỞ DỊCH VỤ INTERNET \e[0m \n"
+printf "\e[1;32m =================================== \e[0m \n"
+sleep 5
+printf "\e[1;32m Đã hoàn thành cài đặt! gõ exit, umount -a và reboot.\e[0m \n"
 
 
 
